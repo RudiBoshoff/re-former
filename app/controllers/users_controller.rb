d7a090761de639b944b3a9cb0d71a4b054bf04f2
Rudi Boshoff <rudiboshoff15@gmail.com>
2019-09-12 12:28:01 +0200

    adds form_for form creation - completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
     def create
+        # MANUAL FORM
+        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        # FORM_TAG and FORM_FOR
+        @user = User.new(user_params)
 
-        # @user = User.new(user_params)
         if @user.save
             redirect_to new_user_path
         else
@@ -12,6 +14,7 @@ class UsersController < ApplicationController
     end
 
     def new
+        @user = User.new
     end
 
     def user_params
